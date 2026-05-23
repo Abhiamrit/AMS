cc7932328792fd0609223a937c9b52cde239fada
Abhiamrit <abhiamritveera@gmail.com>
2026-05-23 10:17:42 +0530

    Revert cookie settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -56,8 +56,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
       maxAge: SESSION_MAX_AGE,
     },
     name: "ams_sid",
